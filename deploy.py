e26aa7451c10c08533114a17f2d10d6711bd4d9c
Jeevan <omborle14@gmail.com>
2026-07-08 14:53:53 +0530

    changes

--- deploy.py
+++ deploy.py
@@ -1,7 +1,32 @@
-import os
 
-print("Starting Deployment")
+import paramiko
 
-os.system("python app.py")
+HOST = os.environ["EC2_HOST"]
+USERNAME = os.environ["EC2_USERNAME"]
+PRIVATE_KEY = os.environ["PRIVATE_KEY"]
 
-print("Deployment Successful")
+client=paramiko.SSHClient()
+
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+client.connect(
+    hostname=HOST,
+    username=USERNAME,
+    key_filename=KEY
+)
+
+commands=[
+    "cd /home/ec2-user/project",
+    "git pull",
+    "python3 app.py"
+]
+
+for command in commands:
+
+    stdin,stdout,stderr=client.exec_command(command)
+
+    print(stdout.read().decode())
+
+client.close()
+
+print("Deployment Completed")
