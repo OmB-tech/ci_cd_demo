dd710c5492f6e33a640cfd8c0c9038da73f21e78
Jeevan <omborle14@gmail.com>
2026-07-08 14:42:16 +0530

    add files

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello from Home")
+# print("Hello from Home")
+import json
+
+with open("students.json", "r") as f:
+    students = json.load(f)
+
+topper = max(students, key=lambda x: x["marks"])
+
+print("Topper:", topper["name"])
+print("Marks:", topper["marks"])
