95e5927ebe1fe34400dfb27d637bc7e91d050e1c
Jeevan <omborle14@gmail.com>
2026-07-08 14:58:14 +0530

    change deploy.py

--- deploy.py
+++ deploy.py
@@ -1,31 +1,34 @@
-
+import os
 import paramiko
 
 HOST = os.environ["EC2_HOST"]
 USERNAME = os.environ["EC2_USERNAME"]
 PRIVATE_KEY = os.environ["PRIVATE_KEY"]
 
-client=paramiko.SSHClient()
+with open("key.pem", "w") as f:
+    f.write(PRIVATE_KEY)
 
+os.chmod("key.pem", 0o600)
+
+client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 client.connect(
     hostname=HOST,
     username=USERNAME,
-    key_filename=KEY
+    key_filename="key.pem"
 )
 
-commands=[
-    "cd /home/ec2-user/project",
-    "git pull",
-    "python3 app.py"
-]
+command = """
+cd /home/ec2-user/aws-python-deployment-demo &&
+git pull &&
+python3 app.py
+"""
 
-for command in commands:
+stdin, stdout, stderr = client.exec_command(command)
 
-    stdin,stdout,stderr=client.exec_command(command)
-
-    print(stdout.read().decode())
+print(stdout.read().decode())
+print(stderr.read().decode())
 
 client.close()
 
